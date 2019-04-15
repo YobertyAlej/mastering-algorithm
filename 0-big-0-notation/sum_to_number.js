432d7fa3aaf3a4636b3826292907fd53f33a9651
Yoberty Alejandro Garcia Valera <gyoberty@gmail.com>
2019-04-15 18:16:15 -0400

    Counting operations

---
+++ 0-big-0-notation/sum_to_number.js
@@ -0,0 +1,37 @@
+/**
+ * Write a function that calculates the sum of all numbers
+ * from 1 up to (and including) n
+ *
+ */
+
+// Fisrt implementation by @YobertyAlej
+const sum = function(n) {
+  let accumulator = 0;
+
+  for (let i = 1; i <= n; i++) {
+    accumulator += i;
+  }
+  return accumulator;
+};
+
+const sumByInstructor = function(n) {
+  return (n * (n + 1)) / 2;
+};
+
+console.log(sum(100)); // This one is slower
+console.log(sumByInstructor(100)); // This one is faster
+
+/**
+ * First one runs slower
+ * Second one runs faster,
+ *
+ * But, it's time a good measurement metric?
+ *
+ * If we count the number of simple operations in each
+ * algorithms we found a CONSTANT number for measuring
+ *
+ * The first algorithm will get us around 5n + 2 operations
+ * The second algorithm will get us exactly 3 operations
+ *
+ * Thus, the second algorithm performs better.
+ */
--- README.md
+++ README.md
@@ -14,3 +14,21 @@ Right there is where Big O' Notation comes through, it offers a way to compare a
 - It helps to talk about your code, and how it performs against others solutions.
 - Useful for discussing about trade-offs between different approaches
 - Allows to identify WHERE your code is being slow down, so it's pretty good for debuging
+
+What does BETTER mean?
+
+We mesuare if a code runs:
+
+- Faster
+- Less memory-intensive
+- More Readable
+
+Faster code:
+
+If you decide to measure your code using time as metric you will find out that it's hard to rely on it.
+
+Mainly because time may differ from machine to machine, and most importantly, the time may differ in the SAME machine.
+
+So, what do we do?
+
+We compare the _number_ of simple operations the computer has to perform
