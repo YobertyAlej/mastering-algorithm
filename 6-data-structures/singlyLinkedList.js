76064e7d71b4786c6436eb18f31b26c50fbff89e
Yoberty Alejandro Garcia Valera <gyoberty@gmail.com>
2019-04-29 18:46:29 -0400

    Singly Linked List implementation

--- 6-data-structures/singlyLinkedList.js
+++ 6-data-structures/singlyLinkedList.js
@@ -16,7 +16,7 @@ class SinglyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-    this.lenght = 0;
+    this.length = 0;
   }
 
   push(value) {
@@ -34,8 +34,67 @@ class SinglyLinkedList {
     }
 
     this.tail = node;
-    this.lenght = this.lenght + 1;
+    this.length = this.length + 1;
 
     return this;
   }
+
+  pop() {
+    /**
+     * If there are no nodes in the list, return undefined
+     * Loop through the list until you reach the tail
+     * Set the next property of the 2nd to last node to be null
+     * Set the tail to be the 2nd to last node
+     */
+
+    if (this.head === null) {
+      return undefined;
+    }
+
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length = this.length - 1;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
+
+  shift() {
+    /**
+     * If no items in node, return undefined
+     * Set the head to be the next property of current head
+     * Decrement the length
+     * Return the previous head
+     */
+
+    if (this.head === null) {
+      return undefined;
+    }
+
+    let current = this.head;
+    this.head = current.next;
+    this.length = this.length - 1;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
+
+module.exports = {
+  Node,
+  SinglyLinkedList
+};
---
+++ 6-data-structures/singlyLinkedList.test.js
@@ -0,0 +1,58 @@
+/* eslint-env jest */
+
+const { SinglyLinkedList } = require("./singlyLinkedList");
+
+let singlyLinked = null;
+
+beforeEach(() => {
+  singlyLinked = new SinglyLinkedList();
+});
+
+afterEach(() => {
+  singlyLinked = null;
+});
+
+describe("# Singly Linked List", () => {
+  it("creates a fresh linked list", () => {
+    expect(singlyLinked).toHaveProperty("head");
+    expect(singlyLinked).toHaveProperty("tail");
+    expect(singlyLinked).toHaveProperty("length");
+
+    expect(singlyLinked.head).toBe(null);
+    expect(singlyLinked.tail).toBe(null);
+    expect(singlyLinked.length).toBe(0);
+  });
+
+  it("push an item to the end", () => {
+    expect(singlyLinked.push(1).tail.value).toBe(1);
+  });
+
+  it("has the same head and tail if only one element", () => {
+    let current = singlyLinked.push(1);
+    expect(current.head).toEqual(current.tail);
+    expect(current.length).toBe(1);
+  });
+
+  it("references the next node", () => {
+    let current = singlyLinked.push(1).push(2);
+    expect(current.head.value).toBe(1);
+    expect(current.head.next.value).toBe(2);
+  });
+
+  it("pops the last item", () => {
+    expect(singlyLinked.push(1).pop().value).toBe(1);
+  });
+
+  it("pops the only item in the list", () => {
+    singlyLinked.push(1).pop();
+    expect(singlyLinked.head).toBe(null);
+    expect(singlyLinked.tail).toBe(null);
+  });
+
+  it("shifts the first item", () => {
+    expect(singlyLinked.push(1).shift().value).toBe(1);
+    singlyLinked.push(1).shift();
+    expect(singlyLinked.head).toBe(null);
+    expect(singlyLinked.tail).toBe(null);
+  });
+});
